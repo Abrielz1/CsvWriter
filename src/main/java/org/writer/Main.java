37a28d39dc51d219824822488c0e47563c6eff87
Abriel <abrielz11@gmail.com>
2024-12-12 13:23:56 +0300

    work half done

--- src/main/java/org/writer/Main.java
+++ src/main/java/org/writer/Main.java
@@ -1,9 +1,12 @@
 package org.writer;
 
 import lombok.SneakyThrows;
-import net.datafaker.Faker;
+import org.writer.model.Person;
 import org.writer.service.ClassManipulator;
+import org.writer.service.Manipulator;
 import org.writer.util.ClassLabel;
+import java.time.LocalDate;
+import java.util.List;
 
 public class Main {
 
@@ -11,11 +14,21 @@ public class Main {
     @SneakyThrows
     public static void main(String[] args) {
 
-        Faker faker = new Faker();
+        Person person1 = Person
+                         .builder()
+                .age(18)
+                .firstName("name")
+                .lastName("last")
+                .dateOfBirth(LocalDate.now().minusYears(15))
+                .build();
 
         ClassManipulator classManipulator = new ClassManipulator();
 
-        classManipulator.classScanner("org.writer.model",
-                (ClassLabel) Class.forName("ClassLabel").getDeclaredConstructor().newInstance());
+        Manipulator manipulator = new Manipulator<>();
+
+      manipulator.manipulate(classManipulator.classScanner("org.writer.model",
+                (ClassLabel) Class.forName("ClassLabel").getDeclaredConstructor().newInstance()), person1);
+
+
     }
 }
--- src/main/java/org/writer/service/Manipulator.java
+++ src/main/java/org/writer/service/Manipulator.java
@@ -1,11 +1,12 @@
 package org.writer.service;
 
-import lombok.RequiredArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
 
-@RequiredArgsConstructor
 public class Manipulator<T> {
 
-    private final ClassManipulator classManipulator;
+    public List<T> manipulate(List<Class<?>> classes, T t) {
 
-
+        return new ArrayList<>();
+    }
 }
